efcba608c846fee360410fe3cdce44143df8b45e
Duc Dao <90080923+ducdmd152@users.noreply.github.com>
2023-05-22 10:27:45 +0700

    Duy Duc | Spring Security Config Successfully
    +) API authentication
    +) API Role authorization

---
+++ backend/src/main/java/com/gifthommie/backend/config/security/MyUserDetails.java
@@ -0,0 +1,63 @@
+package com.gifthommie.backend.config.security;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Set;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.gifthommie.backend.entity.Role;
+import com.gifthommie.backend.entity.User;
+
+public class MyUserDetails implements UserDetails {
+
+	private User user;
+	
+	public MyUserDetails(User user) {
+		this.user = user;
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		String role = user.getAuthority();
+		List<SimpleGrantedAuthority> authorities = new ArrayList<>();
+		
+		authorities.add(new SimpleGrantedAuthority(role));
+		
+		return authorities;
+	}
+
+	@Override
+	public String getPassword() {
+		return user.getPassword();
+	}
+
+	@Override
+	public String getUsername() {
+		return user.getUsername();
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return user.isEnabled();
+	}
+
+}
---
+++ backend/src/main/java/com/gifthommie/backend/config/security/UserDetailsServiceImpl.java
@@ -0,0 +1,28 @@
+package com.gifthommie.backend.config.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import com.gifthommie.backend.entity.User;
+import com.gifthommie.backend.repository.UserRepository;
+
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Override
+	public UserDetails loadUserByUsername(String username) 
+			throws UsernameNotFoundException {
+		User user = userRepository.getUserByUsername(username);
+		
+		if (user == null) {
+			throw new UsernameNotFoundException("Could not find user");
+		}
+		
+		return new MyUserDetails(user);
+	}
+
+}
