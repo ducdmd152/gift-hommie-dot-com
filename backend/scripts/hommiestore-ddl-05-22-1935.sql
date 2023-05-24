718c4fe509105ee89312c28adb2ddc8792910c58
Duc Dao <90080923+ducdmd152@users.noreply.github.com>
2023-05-24 14:15:37 +0700

    Update hommiestore-ddl-05-22-1935.sql

--- backend/scripts/hommiestore-ddl-05-22-1935.sql
+++ backend/scripts/hommiestore-ddl-05-22-1935.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `hommiestore` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `hommiestore`;
 -- MySQL dump 10.13  Distrib 8.0.30, for Win64 (x86_64)
 --
 -- Host: localhost    Database: hommiestore
