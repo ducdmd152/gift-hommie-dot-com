94eb5dd7e2850c299a40382d646d20994af7849c
DangThiemLoc <thiemloc02@gmail.com>
2023-05-29 11:27:19 +0700

    loc-detailStaff-40%

--- frontend/src/pages/manager/ManagerStaffDetailPage.tsx
+++ frontend/src/pages/manager/ManagerStaffDetailPage.tsx
@@ -12,81 +12,86 @@ import staffProductService from "../../services/staff-product-service";
 
 const ManagerStaffDetailPage = () => {
   return (
-
-    <Box border="1px lightgray solid">
-      <VStack flex="1" h="100%" px="8" spacing="4" marginTop='8px'>
-        <Wrap>
-          <Box>
-            {/* <Image
+    <>
+      <Link to="/staff">
+        <Button colorScheme="teal" size="sm" ml="12">
+          {"<< Danh sách nhân viên"}
+        </Button>
+      </Link>
+      <Box border="1px lightgray solid">
+        <VStack flex="1" h="100%" px="8" spacing="4" marginTop='8px'>
+          <Wrap>
+            <Box>
+              {/* <Image
               borderRadius="8px"
               boxSize="120px"
               objectFit="cover"
             // src={productAvatarURL}
             /> */}
-            <WrapItem>
-              <Avatar size='xl' name='' src=''
-                border="1px lightgray solid"
-              />{' '}
-            </WrapItem>
-            <Heading size="sm" textAlign="center" marginBottom="4">
-              @UseName
-            </Heading>
-          </Box>
-        </Wrap>
-      </VStack>
+              <WrapItem>
+                <Avatar size='xl' name='' src=''
+                  border="1px lightgray solid"
+                />{' '}
+              </WrapItem>
+              <Heading size="sm" textAlign="center" marginBottom="4">
+                @UseName
+              </Heading>
+            </Box>
+          </Wrap>
+        </VStack>
 
-      <VStack >
-        <FormControl maxW="600px">
-          <HStack >
-            <FormLabel size="md" fontWeight="bold">
-              FullName
-            </FormLabel>
-            <Input
-              color="gray"
-              // value={product.name}
-              fontWeight="bold"
-            />
-          </HStack>
-        </FormControl>
+        <VStack >
+          <FormControl maxW="600px">
+            <HStack >
+              <FormLabel size="md" fontWeight="bold">
+                FullName
+              </FormLabel>
+              <Input
+                color="gray"
+                // value={product.name}
+                fontWeight="bold"
+              />
+            </HStack>
+          </FormControl>
 
-        <FormControl maxW="600px">
-          <HStack>
-            <FormLabel size="md" fontWeight="bold">
-              UserName
-            </FormLabel>
-            <Input
-              color="gray"
-              // value={product.name}
-              fontWeight="bold"
-            />
-          </HStack>
-        </FormControl>
+          <FormControl maxW="600px">
+            <HStack>
+              <FormLabel size="md" fontWeight="bold">
+                UserName
+              </FormLabel>
+              <Input
+                color="gray"
+                // value={product.name}
+                fontWeight="bold"
+              />
+            </HStack>
+          </FormControl>
 
-        <FormControl maxW="600px">
-          <HStack>
-            <FormLabel size="md" fontWeight="bold">
-              Email
-            </FormLabel>
-            <Input
-              color="gray"
-              // value={product.name}
-              fontWeight="bold"
-            />
-          </HStack>
-        </FormControl>
-        <FormControl maxW="600px">
-          <HStack>
-            <FormLabel size="md" fontWeight="bold">
-              Phone
-            </FormLabel>
-            <Input
-              color="gray"
-              // value={product.name}
-              fontWeight="bold"
-            />
-          </HStack>
-        </FormControl>
-        {/* <FormControl>
+          <FormControl maxW="600px">
+            <HStack>
+              <FormLabel size="md" fontWeight="bold">
+                Email
+              </FormLabel>
+              <Input
+                color="gray"
+                // value={product.name}
+                fontWeight="bold"
+              />
+            </HStack>
+          </FormControl>
+          <FormControl maxW="600px">
+            <HStack>
+              <FormLabel size="md" fontWeight="bold">
+                Phone
+              </FormLabel>
+              <Input
+                color="gray"
+                // value={product.name}
+                fontWeight="bold"
+              />
+            </HStack>
+          </FormControl>
+          {/* <FormControl>
           <FormLabel size="md" fontWeight="bold">
             Gender
           </FormLabel>
@@ -98,35 +103,36 @@ const ManagerStaffDetailPage = () => {
             </Stack>
           </RadioGroup>
         </FormControl> */}
-        <FormControl maxW="600px" >
-          <HStack>
-            <FormLabel size="md" fontWeight="bold">
-              Birth Year
-            </FormLabel>
-            <Input
-              placeholder="YYYY"
-              color="gray"
-              // value={product.name}
-              fontWeight="bold"
-            />
-          </HStack>
-        </FormControl>
-      </VStack>
+          <FormControl maxW="600px" >
+            <HStack>
+              <FormLabel size="md" fontWeight="bold">
+                Birth Year
+              </FormLabel>
+              <Input
+                placeholder="YYYY"
+                color="gray"
+                // value={product.name}
+                fontWeight="bold"
+              />
+            </HStack>
+          </FormControl>
+        </VStack>
 
-      <HStack justifyContent='center' marginTop='10px'>
-        <Link to={"/staff/edit"}>
-          <Button colorScheme="blue" size="md">
-            Chỉnh sửa
+        <HStack justifyContent='center' marginTop='10px'>
+          <Link to={"/staff/edit"}>
+            <Button colorScheme="blue" size="md">
+              Chỉnh sửa
+            </Button>
+          </Link>
+          <Button
+            colorScheme="red"
+            size="md"
+          >
+            Xóa
           </Button>
-        </Link>
-        <Button
-          colorScheme="red"
-          size="md"
-        >
-          Xóa
-        </Button>
-      </HStack>
-    </Box>
+        </HStack>
+      </Box>
+    </>
   );
 
 
