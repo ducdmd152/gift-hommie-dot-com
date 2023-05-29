fea3cbf3a644c75933b3b215742bedba879a600f
nguyentan <van59116@gmail.com>
2023-05-29 23:35:42 +0700

    Done get user profile

--- backend/src/main/java/com/gifthommie/backend/controller/CustomerAccountController.java
+++ backend/src/main/java/com/gifthommie/backend/controller/CustomerAccountController.java
@@ -1,6 +1,8 @@
 package com.gifthommie.backend.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,7 +11,9 @@ import org.springframework.web.bind.annotation.RestController;
 import com.gifthommie.backend.dto.ProductRequestDTO;
 import com.gifthommie.backend.dto.UserProfileDTO;
 import com.gifthommie.backend.entity.User;
+import com.gifthommie.backend.exception.NotFoundException;
 import com.gifthommie.backend.service.UserService;
+import com.gifthommie.backend.utils.SecurityUtils;
 
 @RestController
 @RequestMapping("/customer/account")
@@ -39,6 +43,12 @@ public class CustomerAccountController {
 		
 		password hiện tại đang để default BCrypt là 123456
 	 */
+	@GetMapping()
+	public User getUser() {
+		User user = SecurityUtils.getPrincipal().getUser();
+		String email = user.getEmail();
+		return userService.getUserByEmail(email);
+	}
 	@PutMapping()
 	public User updateUserDTO(@RequestBody UserProfileDTO userRequestDTO) {
 		User result = userService.updateUserProfileDTO(userRequestDTO);
