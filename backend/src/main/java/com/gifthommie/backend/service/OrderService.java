8724351d0826f7f840bdea807ad7db1c1da708ac
QuyetTran1112 <133133145+QuyetTran1112@users.noreply.github.com>
2023-06-08 10:43:51 +0700

    Revert "DONE View orders placed by customers (with pagination)"
    This reverts commit ec2770f45aefe7b3f7d6afb18db70a5c74a5e041.

--- backend/src/main/java/com/gifthommie/backend/service/OrderService.java
+++ backend/src/main/java/com/gifthommie/backend/service/OrderService.java
@@ -2,15 +2,11 @@ package com.gifthommie.backend.service;
 
 import java.util.List;
 
-import com.gifthommie.backend.dto.APIPageableResponseDTO;
 import com.gifthommie.backend.entity.Orders;
-import com.gifthommie.backend.entity.Product;
 
 public interface OrderService {
 	
 	public Orders getOrderByOrderId(Integer orderId);
 	
 	public List<Orders> getOrderListWithoutStatus(String[] tmp);
-	
-	APIPageableResponseDTO<Orders> getPageableOrder(Integer pageNo, Integer pageSize, String status);
 }
