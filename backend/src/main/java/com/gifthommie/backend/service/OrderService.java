4c74adbeaed7e908a60c1e907e4e8c5d117e91e2
QuyetTran1112 <133133145+QuyetTran1112@users.noreply.github.com>
2023-06-08 11:27:09 +0700

    DONE View orders placed by customers (with pagination)
    - Make sure name Orders Table in Database is Order
    - Make sure collum "status" in  Orders Table have data  [PENDING, CONFIRMED, DELIVERYING, SUCCSESSFUL, CANCELED]
    
    Co-Authored-By: Nguyễn Tấn <76957209+tanlionel@users.noreply.github.com>

--- backend/src/main/java/com/gifthommie/backend/service/OrderService.java
+++ backend/src/main/java/com/gifthommie/backend/service/OrderService.java
@@ -2,6 +2,7 @@ package com.gifthommie.backend.service;
 
 import java.util.List;
 
+import com.gifthommie.backend.dto.APIPageableResponseDTO;
 import com.gifthommie.backend.dto.OrderResponseDTO;
 import com.gifthommie.backend.entity.Orders;
 
@@ -12,4 +13,6 @@ public interface OrderService {
 	public List<Orders> getOrderListWithoutStatus(String[] tmp);
 	
 	public OrderResponseDTO save(float totalPrice, String email);
+	
+	APIPageableResponseDTO<Orders> getPageableOrder(Integer pageNo, Integer pageSize, String status);
 }
--- backend/src/main/java/com/gifthommie/backend/service/OrderServiceImpl.java
+++ backend/src/main/java/com/gifthommie/backend/service/OrderServiceImpl.java
@@ -4,8 +4,11 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
+import com.gifthommie.backend.dto.APIPageableResponseDTO;
 import com.gifthommie.backend.dto.OrderResponseDTO;
 import com.gifthommie.backend.entity.Orders;
 import com.gifthommie.backend.repository.OrderRepository;
@@ -42,6 +45,13 @@ public class OrderServiceImpl implements OrderService{
 		orderResponseDTO.setTotalPrice(totalPrice);
 		
 		return orderResponseDTO;
+	}
+	
+	
+	@Override
+	public APIPageableResponseDTO<Orders> getPageableOrder(Integer pageNo, Integer pageSize, String status) {
+		Page<Orders> page  = orderRepository.getOrderedWithStatus(status, PageRequest.of(pageNo, pageSize));
+		return new APIPageableResponseDTO<Orders>(page);
 	}
 
 }
