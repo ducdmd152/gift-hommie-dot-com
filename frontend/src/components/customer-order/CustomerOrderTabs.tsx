569005ffdce1888c890ec01e32e73b0b7e58c9ca
Duc Dao <90080923+ducdmd152@users.noreply.github.com>
2023-06-12 19:03:15 +0700

    FE | +Tabs

---
+++ frontend/src/components/customer-order/CustomerOrderTabs.tsx
@@ -0,0 +1,65 @@
+import {
+  Box,
+  Tab,
+  TabIndicator,
+  TabList,
+  TabPanel,
+  TabPanels,
+  Tabs,
+} from "@chakra-ui/react";
+import React from "react";
+
+const CustomerOrderTabs = () => {
+  return (
+    <Box paddingX="8">
+      <Tabs isFitted variant="enclosed">
+        <TabList>
+          <Tab
+            className="customer-order-tab"
+            _selected={{ color: "white", bg: "teal.600" }}
+          >
+            Tất cả
+          </Tab>
+          <Tab
+            className="customer-order-tab"
+            _selected={{ color: "white", bg: "yellow.500" }}
+          >
+            Chờ xác nhận
+          </Tab>
+          <Tab
+            className="customer-order-tab"
+            _selected={{ color: "white", bg: "blue.400" }}
+          >
+            Đã xác nhận
+          </Tab>
+          <Tab
+            className="customer-order-tab"
+            _selected={{ color: "white", bg: "green.300" }}
+          >
+            Đang vận chuyển
+          </Tab>
+          <Tab
+            className="customer-order-tab"
+            _selected={{ color: "white", bg: "green.400" }}
+          >
+            Đã hoàn thành
+          </Tab>
+          <Tab
+            className="customer-order-tab"
+            _selected={{ color: "white", bg: "gray.400" }}
+          >
+            Đã hủy
+          </Tab>
+        </TabList>
+        <TabIndicator
+          mt="-1.5px"
+          height="2px"
+          bg="blue.500"
+          borderRadius="1px"
+        />
+      </Tabs>
+    </Box>
+  );
+};
+
+export default CustomerOrderTabs;
