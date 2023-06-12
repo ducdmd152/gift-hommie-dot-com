aa2d55a37612c6bdb9d5f6df26579c34250115f5
Duc Dao <90080923+ducdmd152@users.noreply.github.com>
2023-06-12 21:14:29 +0700

    FE | UI | CustomerOrderDetail 70%

---
+++ frontend/src/components/customer-order/CustomerOrderDetailInfo.tsx
@@ -0,0 +1,163 @@
+import React, { useState } from "react";
+import useFetchCart, { CartQuery } from "../../hooks/useFetchCart";
+import { Badge, Card, HStack, Heading, VStack, Text } from "@chakra-ui/react";
+
+const CustomerOrderDetailInfo = () => {
+  //CODE FAKE DATA (TEMPORARY)
+  const [cartQuery, setCartQuery] = useState({} as CartQuery);
+  const { carts, pageable, setCarts } = useFetchCart(cartQuery);
+  // GET DATA
+  let items = carts;
+  return (
+    <Card>
+      <VStack w="100%">
+        {/* HEADER INFO */}
+        <HStack w="100%" justifyContent={"space-between"} spacing="0">
+          <Badge flex="1" p="2" fontSize={"lg"}>
+            Chi tiết đơn hàng
+            {" | ID >> 01"}
+          </Badge>
+          <Badge
+            flex="1"
+            p="2"
+            fontSize={"lg"}
+            textAlign={"right"}
+            className="none-text-transform"
+          >
+            <HStack w="100%" justifyContent="flex-end">
+              <Text>Trạng thái đơn hàng: </Text>
+              <Badge
+                colorScheme="yellow"
+                fontSize={"lg"}
+                className="none-text-transform"
+              >
+                {" "}
+                PENDING | Chờ xác nhận
+              </Badge>
+            </HStack>
+          </Badge>
+        </HStack>
+
+        {/* CUSTOMER INFO */}
+        <Card p="4" w="100%">
+          <Heading size="sm" mb="2" color="gray">
+            Người nhận
+          </Heading>
+          <VStack w="100%" alignItems="flex-start">
+            <Text fontWeight={"bold"}>TRẦN CƯƠNG QUYẾT </Text>
+            <Text>
+              {" "}
+              Điện thoại: <strong>01214212412</strong>
+            </Text>
+            <Text>
+              Địa chỉ: <strong>TX25, P.THẠNH XUÂN, Q12,THÀNH PHỐ HCM</strong>{" "}
+            </Text>
+          </VStack>
+        </Card>
+
+        <Card p="4" w="100%">
+          <HStack
+            w="100%"
+            justifyContent={"space-between"}
+            alignItems={"stretch"}
+          >
+            <Card background="blue.100" flex="1" p="4">
+              <VStack>
+                <HStack spacing="4" w="100%">
+                  <Text fontSize="md" flex="2" textAlign="left">
+                    Tổng thanh toán
+                  </Text>
+                  <Text
+                    fontSize="lg"
+                    textAlign="right"
+                    flex="1"
+                    fontWeight="bold"
+                  >
+                    {items.reduce((acc, item) => acc + item.total, 0) / 1000}
+                    {".000đ"}
+                  </Text>
+                </HStack>
+                <HStack spacing="4" w="100%">
+                  <Text fontSize="md" flex="2" textAlign="left">
+                    Phí vận chuyển
+                  </Text>
+                  <Text
+                    fontSize="lg"
+                    textAlign="right"
+                    flex="1"
+                    fontWeight="bold"
+                  >
+                    Freeship
+                  </Text>
+                </HStack>
+                <HStack spacing="4" w="100%">
+                  <Text fontSize="md" flex="2" textAlign="left">
+                    Tổng thanh toán
+                  </Text>
+                  <Text
+                    fontSize="lg"
+                    textAlign="right"
+                    flex="1"
+                    fontWeight="bold"
+                  >
+                    {items.reduce((acc, item) => acc + item.total, 0) / 1000}
+                    {".000đ"}
+                  </Text>
+                </HStack>
+                <HStack spacing="4" w="100%">
+                  <Text fontSize="md" flex="2" textAlign="left">
+                    Thanh toán khi nhận hàng
+                  </Text>
+                  <Text
+                    fontSize="lg"
+                    textAlign="right"
+                    flex="1"
+                    fontWeight="bold"
+                  >
+                    {items.reduce((acc, item) => acc + item.total, 0) / 1000}
+                    {".000đ"}
+                  </Text>
+                </HStack>
+              </VStack>
+            </Card>
+            {/* METHODS */}
+            <Card background="gray.100" flex="1" p="4">
+              {/* SUMMARY INFO */}
+              <VStack>
+                <HStack spacing="4" w="100%">
+                  <Text fontSize="md" flex="2" textAlign="left">
+                    Hình thức thanh toán
+                  </Text>
+                  <Text
+                    fontSize="lg"
+                    textAlign="right"
+                    flex="3"
+                    fontWeight="bold"
+                  >
+                    Thanh toán khi nhận hàng (COD)
+                  </Text>
+                </HStack>
+                <HStack spacing="4" w="100%">
+                  <Text fontSize="md" flex="2" textAlign="left">
+                    Hình thức vận chuyển
+                  </Text>
+                  <Text
+                    fontSize="lg"
+                    textAlign="right"
+                    flex="3"
+                    fontWeight="bold"
+                  >
+                    Giao hàng tiêu chuẩn (nhanh)
+                  </Text>
+                </HStack>
+              </VStack>
+            </Card>
+            {/* SUMMARY INFO */}
+          </HStack>
+        </Card>
+      </VStack>
+    </Card>
+  );
+};
+
+export default CustomerOrderDetailInfo;
--- frontend/src/pages/customer/CustomerOrderDetailPage.tsx
+++ frontend/src/pages/customer/CustomerOrderDetailPage.tsx
@@ -1,11 +1,18 @@
 import React from "react";
 import useFetchCart, { CartQuery } from "../../hooks/useFetchCart";
 import CustomerOrderItems from "../../components/customer-order/CustomerOrderItems";
-import { Box } from "@chakra-ui/react";
+import { Box, Card, Heading } from "@chakra-ui/react";
+import CustomerOrderDetailInfo from "../../components/customer-order/CustomerOrderDetailInfo";
 const CustomerOrderDetailPage = () => {
   return (
-    <Box>
-      <CustomerOrderItems />
+    <Box paddingX="8">
+      <CustomerOrderDetailInfo />
+      <Card p="4" w="100%" mt="4">
+        <Heading size="md" textAlign={"center"} mb="4">
+          Thông tin sản phẩm
+        </Heading>
+        <CustomerOrderItems />
+      </Card>
     </Box>
   );
 };
