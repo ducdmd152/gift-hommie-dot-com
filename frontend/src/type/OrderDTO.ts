159b7c73564c2535e1289180d88f44b2fffee0eb
Duc Dao <90080923+ducdmd152@users.noreply.github.com>
2023-06-14 19:57:51 +0700

    Data Convention Updating

---
+++ frontend/src/type/OrderDTO.ts
@@ -0,0 +1,32 @@
+import OrderDetailDTO from "./OrderDetailDTO";
+import UserDTO from "./UserDTO";
+
+interface OrderDTO {
+  // personal inf
+  name: string;
+  phone: string;
+
+  address: string;
+  // = checkoutDTO.address + " " + checkoutDTO.wardName + + checkoutDTO.districtName + + checkoutDTO.provinceName
+  wardCode: number;
+  districtID: number;
+  provinceID: number;
+
+  message: string;
+
+  orderDetails: OrderDetailDTO[];
+
+  paymentMethod: number; // ~ paymentId
+
+  shippingFee: number;
+  shippingMethod: number;
+
+  user: UserDTO; // The user order
+
+  status: string; // PENDING CANCELLED REFUSED
+  // CONFIRMED DELIVERING SUCCESSFUL FAIL
+  // AUTO => PENDING WHEN CREATING
+  comment: string;
+  orderTime: string; // Auto-update by BE when CREATE ORDER
+  lastUpdatedTime: string; // Auto-update by BE when CREATE/UPDATE ORDER
+}
--- frontend/src/type/OrderDetailDTO.tsx
+++ frontend/src/type/OrderDetailDTO.tsx
@@ -4,7 +4,10 @@ export default interface CategoryDTO {
   id: number;
   orderID: number;
   productId: number;
+  price: number;
   quantity: number;
   product: ProductDTO;
   total: number;
+  // tạo một hàm getTotal { return quantity*price }
+  // => auto có field total trong json
 }
