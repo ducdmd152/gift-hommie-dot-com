8bc3d8c735150e00eb31ae9de10c704ba6f48a94
Duc Dao <90080923+ducdmd152@users.noreply.github.com>
2023-06-14 11:12:56 +0700

    Paypal | Add successful demo

---
+++ frontend/src/components/checkout/CheckoutPaymentModal.tsx
@@ -0,0 +1,110 @@
+import React, { useEffect } from "react";
+import CheckoutDTO from "../../type/CheckoutDTO";
+import {
+  Button,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  Text,
+} from "@chakra-ui/react";
+import {
+  PayPalButtons,
+  PayPalScriptProvider,
+  usePayPalScriptReducer,
+} from "@paypal/react-paypal-js";
+
+const rate = 1 / 23800;
+const currency = "USD";
+const style = { layout: "vertical" };
+
+interface Props {
+  isOpen: boolean;
+  onOpen: () => void;
+  onClose: () => void;
+  checkoutData: CheckoutDTO;
+  setCheckoutData: (data: CheckoutDTO) => void;
+}
+const CheckoutPaymentModal = ({
+  isOpen,
+  onOpen,
+  onClose,
+  checkoutData,
+  setCheckoutData,
+}: Props) => {
+  let total = checkoutData.carts.reduce((acc, item) => acc + item.total, 0);
+  let shippingFee = checkoutData.shippingFee ? checkoutData.shippingFee : 0;
+  let sum = total + shippingFee;
+  // PAYPAL handling
+
+  return (
+    <Modal isOpen={isOpen} onClose={onClose} preserveScrollBarGap={false}>
+      <ModalOverlay />
+      <ModalContent style={{ overflowY: "scroll", height: "90vh" }}>
+        <ModalHeader>Thanh toán qua Paypal</ModalHeader>
+        <ModalCloseButton />
+        <ModalBody>
+          <Text
+            fontSize="lg"
+            textAlign="center"
+            fontWeight="bold"
+            mb="4"
+            color="orange"
+          >
+            Click vào nút bên dưới để tiến hành thanh toán
+          </Text>
+          <PayPalScriptProvider
+            options={{
+              clientId:
+                "ARoi3O0eCaY4PgNsrZxTJklW9GbaWekKLptBbN6PXhZ4US6fIYkInRUJ65X93zScKp1pyZSCLLqDTZqx",
+            }}
+          >
+            <PayPalButtonWrapper amount={sum * rate} />
+          </PayPalScriptProvider>
+        </ModalBody>
+
+        <ModalFooter>
+          <Button colorScheme="red" variant="outline" mr={3} onClick={onClose}>
+            Hủy
+          </Button>
+          {/* <Button variant="ghost">Secondary Action</Button> */}
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
+  );
+};
+
+const PayPalButtonWrapper = ({ amount }: { amount: number }) => {
+  // usePayPalScriptReducer can be use only inside children of PayPalScriptProviders
+  // This is the main reason to wrap the PayPalButtons in a new component
+  const [{ options, isPending }, dispatch] = usePayPalScriptReducer();
+  console.log(amount.toFixed(2));
+
+  return (
+    <PayPalButtons
+      createOrder={(data, actions) => {
+        return actions.order
+          .create({
+            purchase_units: [
+              {
+                amount: {
+                  currency_code: "USD",
+                  value: amount.toFixed(2),
+                },
+              },
+            ],
+          })
+          .then((orderId) => {
+            // Your code here after create the order
+            return orderId;
+          });
+        // .catch((error) => {});
+      }}
+    />
+  );
+};
+
+export default CheckoutPaymentModal;
--- frontend/src/components/checkout/CheckoutSummary.tsx
+++ frontend/src/components/checkout/CheckoutSummary.tsx
@@ -1,3 +1,4 @@
+import { PayPalScriptProvider, PayPalButtons } from "@paypal/react-paypal-js";
 import { HStack, VStack, Text } from "@chakra-ui/layout";
 import React, { useContext } from "react";
 import CheckoutPaymentSelector from "./CheckoutPaymentSelector";
@@ -6,68 +7,82 @@ import { Card } from "@chakra-ui/card";
 import CheckoutShippingMethod from "./CheckoutShippingMethod";
 import { GLOBAL_CONTEXT } from "../../App";
 import CheckoutDTO from "../../type/CheckoutDTO";
+import { Box, useDisclosure } from "@chakra-ui/react";
+import CheckoutPaymentModal from "./CheckoutPaymentModal";
 interface Props {
   checkoutData: CheckoutDTO;
   setCheckoutData: (data: CheckoutDTO) => void;
 }
 const CheckoutSummary = ({ checkoutData, setCheckoutData }: Props) => {
-  const selectedCartContext = useContext(GLOBAL_CONTEXT).selectedCartContext;
-  let items = selectedCartContext.getItems();
+  let items = checkoutData.carts;
+  const { isOpen, onOpen, onClose } = useDisclosure();
   let total = items.reduce((acc, item) => acc + item.total, 0);
   let shippingFee = checkoutData.shippingFee ? checkoutData.shippingFee : 0;
   let sum = total + shippingFee;
   return (
-    <Card p={2}>
-      <CheckoutPaymentSelector
-        checkoutData={checkoutData}
-        setCheckoutData={setCheckoutData}
-      />
-      <CheckoutShippingMethod
-        checkoutData={checkoutData}
-        setCheckoutData={setCheckoutData}
-      />
-      <Card marginTop="2" p="4">
-        {/* SUMMARY INFO */}
-        <VStack>
-          <HStack spacing="4" w="100%">
-            <Text fontSize="md" flex="2" textAlign="left">
-              Tổng tiền hàng
-            </Text>
-            <Text fontSize="lg" textAlign="right" flex="1" fontWeight="bold">
-              {(total / 1000).toFixed(3) + "đ"}
-            </Text>
-          </HStack>
-          <HStack spacing="4" w="100%">
-            <Text fontSize="md" flex="2" textAlign="left">
-              Phí vận chuyển
-            </Text>
-            <Text fontSize="lg" textAlign="right" flex="1" fontWeight="bold">
-              {(shippingFee / 1000).toFixed(3) + "đ"}
-            </Text>
-          </HStack>
-          <HStack spacing="4" w="100%">
-            <Text fontSize="md" flex="2" textAlign="left">
-              Thành tiền
-            </Text>
-            <Text fontSize="lg" textAlign="right" flex="1" fontWeight="bold">
-              {(sum / 1000).toFixed(3) + "đ"}
-            </Text>
-          </HStack>
-        </VStack>
-      </Card>
+    <Box>
+      <Card p={2}>
+        <CheckoutPaymentSelector
+          checkoutData={checkoutData}
+          setCheckoutData={setCheckoutData}
+        />
+        <CheckoutShippingMethod
+          checkoutData={checkoutData}
+          setCheckoutData={setCheckoutData}
+        />
+        <Card marginTop="2" p="4">
+          {/* SUMMARY INFO */}
+          <VStack>
+            <HStack spacing="4" w="100%">
+              <Text fontSize="md" flex="2" textAlign="left">
+                Tổng tiền hàng
+              </Text>
+              <Text fontSize="lg" textAlign="right" flex="1" fontWeight="bold">
+                {(total / 1000).toFixed(3) + "đ"}
+              </Text>
+            </HStack>
+            <HStack spacing="4" w="100%">
+              <Text fontSize="md" flex="2" textAlign="left">
+                Phí vận chuyển
+              </Text>
+              <Text fontSize="lg" textAlign="right" flex="1" fontWeight="bold">
+                {(shippingFee / 1000).toFixed(3) + "đ"}
+              </Text>
+            </HStack>
+            <HStack spacing="4" w="100%">
+              <Text fontSize="md" flex="2" textAlign="left">
+                Thành tiền
+              </Text>
+              <Text fontSize="lg" textAlign="right" flex="1" fontWeight="bold">
+                {(sum / 1000).toFixed(3) + "đ"}
+              </Text>
+            </HStack>
+          </VStack>
+        </Card>
 
-      <HStack justifyContent={"center"}>
-        <Button
-          colorScheme="blue"
-          size="lg"
-          marginTop="4"
-          paddingX="8"
-          w="100%"
-        >
-          Đặt hàng
-        </Button>
-      </HStack>
-    </Card>
+        <HStack justifyContent={"center"}>
+          <Button
+            colorScheme="blue"
+            size="lg"
+            marginTop="4"
+            paddingX="8"
+            w="100%"
+            onClick={() => {
+              onOpen();
+            }}
+          >
+            Đặt hàng
+          </Button>
+        </HStack>
+      </Card>
+      <CheckoutPaymentModal
+        checkoutData={checkoutData}
+        setCheckoutData={setCheckoutData}
+        isOpen={isOpen}
+        onOpen={onOpen}
+        onClose={onClose}
+      />
+    </Box>
   );
 };
 
