8330f61c6094d1f5cc4716c175c7f5148f4b2758
QuyetTran1112 <133133145+QuyetTran1112@users.noreply.github.com>
2023-06-20 13:39:41 +0700

    Update ForgotPasswordController.java

--- backend/src/main/java/com/gifthommie/backend/controller/ForgotPasswordController.java
+++ backend/src/main/java/com/gifthommie/backend/controller/ForgotPasswordController.java
@@ -1,7 +1,5 @@
 package com.gifthommie.backend.controller;
 
-import java.text.MessageFormat;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
@@ -10,6 +8,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.gifthommie.backend.entity.User;
 import com.gifthommie.backend.service.UserService;
 
 import net.bytebuddy.utility.RandomString;
@@ -25,12 +24,12 @@ public class ForgotPasswordController {
 	JavaMailSender mailSender;
 	
 	@PostMapping("/{email}")
-	public String process_forgotPassword (@PathVariable String email) {
-		
+	public String process_forgotPassword (@PathVariable String email) {	
 		String token  = RandomString.make(7);
+		User u = new User();
 		try {
 			userService.updateResetPassword(token, email);
-			
+			u = userService.getUserByEmail(email);
 			// Phần url tạm để là local host
 			String resetPasswordLink = "http://localhost:8080/account/forgotPassword";
 			SimpleMailMessage message = new SimpleMailMessage();
@@ -39,23 +38,18 @@ public class ForgotPasswordController {
 			    
 			    // Phần này là chổ gửi kèm link và code token
 			    //message.setText(MessageFormat.format(resetPasswordLink, token));
-			    message.setText("Chào bạn, reset mật khẩu đê token nè " + token + " Đây là link : " + resetPasswordLink);
-			    mailSender.send(message);
-			
-			
+			    message.setText("Chào bạn, reset mật khẩu đê, token nè: [ " + token + " ]. Token sẽ hết hạn vào lúc: " + u.getExpired_verification_code() );
+			    
+			    mailSender.send(message);			
 		} catch (Exception e) {
 			return e.getMessage();
 			
 		}
 		
 		// Phần này là link nhận reset password
-		// send email
-		
-		
-		
-		
+		// send email	
 		// return này để test xem email và token in ra như thế nào
-		return email +" " + token;
+		return email +"-"+ token +"-"+ u.getExpired_verification_code();
 	}
 	
 	
