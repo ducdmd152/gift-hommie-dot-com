6cac8d9e8d718ae307c0872b68eb062135f0249c
thien123dh <134196273+thien123dh@users.noreply.github.com>
2023-06-25 17:01:45 +0700

    DONE PRODUCT AND REVENUE STATISTIC

--- backend/src/main/java/com/gifthommie/backend/service/ProductStatisticServiceImpl.java
+++ backend/src/main/java/com/gifthommie/backend/service/ProductStatisticServiceImpl.java
@@ -6,14 +6,11 @@ import java.time.LocalDateTime;
 import java.time.YearMonth;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.HashMap;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.gifthommie.backend.dto.RevenueDTO;
 import com.gifthommie.backend.dto.ProductStatisticDTO;
 import com.gifthommie.backend.dto.ProductStatisticDTO.Day;
 import com.gifthommie.backend.dto.ProductStatisticDTO.Month;
@@ -38,6 +35,7 @@ public class ProductStatisticServiceImpl implements ProductStatisticService {
 	private final int WEEKS_OF_MONTH = 4;
 	private final int MONTHS_OF_QUARTER = 4;
 	private final String DATE_PATTERN = "yyyy-MM-dd HH:mm:ss";
+	private final String ORDER_SUCCESSFUL = "SUCCESSFUL";
 
 	private LocalDateTime convertStringToLocalDateTime(String date) {
 		DateTimeFormatter formatter = DateTimeFormatter.ofPattern(DATE_PATTERN);
@@ -47,62 +45,31 @@ public class ProductStatisticServiceImpl implements ProductStatisticService {
 	}
 
 	private ProductStatisticValue getProductStatisticFromTo(LocalDateTime startDate, LocalDateTime endDate) {
-		List<Orders> list = orderRepository.findSuccessfulOrdersFromTo(startDate, endDate);
-	    HashMap<Integer, Integer> sale = new HashMap<>();
-	    HashMap<Integer, Float> rating = new HashMap<>();
-
 		int sold = 0;
-
+		
 		List<Product> topSale = new ArrayList<>();
 		List<Product> topRating = new ArrayList<>();
-
-		if (list != null)
-			for (Orders o : list)
-				for (OrderDetail od : o.getOrderDetails()) {
-					sold += od.getQuantity();
-					int productId = od.getProductId();
-
-					// SALE
-					if (sale.get(productId) == null)
-						sale.put(productId, 0);
-					sale.put(productId, sale.get(productId) + 1);
-					
-					// RATING
-					if (rating.get(productId) == null)
-						rating.put(productId, 0F);
-					
-					rating.put(productId, rating.get(productId) + od.getRating());
-				}
 		
-		for (int productId : rating.keySet())
-			rating.put(productId, rating.get(productId) / sale.get(productId));
-		
-		for (int productId : rating.keySet()) {
+		for (Integer productId : orderRepository.findTopSoldProductFromTo(startDate, endDate)) {
 			Product p = productRepository.findProductById(productId, ENABLED);
-			
-			p.setSold(sale.get(productId));
-			p.setRating(rating.get(productId));
-			
+			p.setSold(orderRepository.getOrderedQuantityByProductIdFromTo(productId, ORDER_SUCCESSFUL, startDate, endDate));
+			p.setRating(orderRepository.getAverageRatingByProductIdFromTo(sold, startDate, endDate));
 			topSale.add(p);
+			sold += p.getSold();
+		}
+
+		for (Integer productId : orderRepository.findTopRatingProductFromTo(startDate, endDate)) {
+			
+			Product p = productRepository.findProductById(productId, ENABLED);
+			p.setSold(orderRepository.getOrderedQuantityByProductIdFromTo(productId, ORDER_SUCCESSFUL, startDate, endDate));
+			p.setRating(orderRepository.getAverageRatingByProductIdFromTo(sold, startDate, endDate));
+			
 			topRating.add(p);
 		}
 		
-		topSale.sort(new Comparator<Product>() {
-			@Override
-			public int compare(Product o1, Product o2) {
-				return o2.getSold().compareTo(o1.getSold());
-			}
-		});
-		
-		topRating.sort(new Comparator<Product>() {
-			public int compare(Product o1, Product o2) {
-				return o2.getRating().compareTo(o1.getRating());
-			};
-		});
-		
 		return new ProductStatisticValue(sold, topSale, topRating);
 	}
-
+	
 	public Day getProductStatisticOfDay(String date) {
 		LocalDateTime startDate = convertStringToLocalDateTime(date);
 		LocalDateTime endDate = startDate.plusDays(1);
@@ -199,4 +166,60 @@ public class ProductStatisticServiceImpl implements ProductStatisticService {
 		
 		return productStatisticDTO;
 	}
+	
+//	private ProductStatisticValue getProductStatisticFromTo(LocalDateTime startDate, LocalDateTime endDate) {
+//	List<Orders> list = orderRepository.findSuccessfulOrdersFromTo(startDate, endDate);
+//    HashMap<Integer, Integer> sale = new HashMap<>();
+//    HashMap<Integer, Float> rating = new HashMap<>();
+//
+//	int sold = 0;
+//
+//	List<Product> topSale = new ArrayList<>();
+//	List<Product> topRating = new ArrayList<>();
+//
+//	if (list != null)
+//		for (Orders o : list)
+//			for (OrderDetail od : o.getOrderDetails()) {
+//				sold += od.getQuantity();
+//				int productId = od.getProductId();
+//
+//				// SALE
+//				if (sale.get(productId) == null)
+//					sale.put(productId, 0);
+//				sale.put(productId, sale.get(productId) + 1);
+//				
+//				// RATING
+//				if (rating.get(productId) == null)
+//					rating.put(productId, 0F);
+//				rating.put(productId, rating.get(productId) + od.getRating());
+//			}
+//	
+//	for (int productId : rating.keySet())
+//		rating.put(productId, rating.get(productId) / sale.get(productId));
+//	
+//	for (int productId : rating.keySet()) {
+//		Product p = productRepository.findProductById(productId, ENABLED);
+//		
+//		p.setSold(sale.get(productId));
+//		p.setRating(rating.get(productId));
+//		
+//		topSale.add(p);
+//		topRating.add(p);
+//	}
+//	
+//	topSale.sort(new Comparator<Product>() {
+//		@Override
+//		public int compare(Product o1, Product o2) {
+//			return o2.getSold().compareTo(o1.getSold());
+//		}
+//	});
+//	
+//	topRating.sort(new Comparator<Product>() {
+//		public int compare(Product o1, Product o2) {
+//			return o2.getRating().compareTo(o1.getRating());
+//		};
+//	});
+//	
+//	return new ProductStatisticValue(sold, topSale, topRating);
+//}
 }
