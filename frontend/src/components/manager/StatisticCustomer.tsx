6065c856cb1a9addbab79f8df8a70049577545b9
Duc Dao <90080923+ducdmd152@users.noreply.github.com>
2023-06-26 11:05:48 +0700

    Completed 70% UI for statistics

--- frontend/src/components/manager/StatisticCustomer.tsx
+++ frontend/src/components/manager/StatisticCustomer.tsx
@@ -3,12 +3,13 @@ import { Image } from "@chakra-ui/image";
 import { Badge, Box, HStack, Text } from "@chakra-ui/layout";
 import React from "react";
 import utilService from "../../services/util-service";
+import { Avatar } from "@chakra-ui/avatar";
 
 const StatisticCustomer = () => {
   return (
     <Box>
-      <Card flex="1" p="4">
-        <Text fontSize="xl">Lượt sản phẩm bán trong ngày</Text>
+      {/* <Card flex="1" p="4">
+        <Text fontSize="xl">Khách hàng đặt đơn hôm nay</Text>
         <HStack w="100%" mt="4">
           <Card w="100%" p="2">
             <HStack justifyContent={"space-between"}>
@@ -38,19 +39,19 @@ const StatisticCustomer = () => {
             </HStack>
           </Card>
         </HStack>
-      </Card>
+      </Card> */}
       <HStack mt="2">
         <Card flex="1" p="4">
-          <Text fontSize="xl">Top sản phẩm bán chạy</Text>
+          <Text fontSize="xl">Top khách hàng tiềm năng</Text>
           <Text fontSize="md" color="gray" fontStyle={"italic"}>
-            (trong tháng)
+            (theo tổng đơn)
           </Text>
           <HStack w="100%" mt="4">
             <Card w="100%" p="2">
               <HStack justifyContent={"space-between"}>
                 <HStack spacing="3" className="product-card">
-                  <Image
-                    borderRadius={"8px"}
+                  <Avatar
+                    // borderRadius={"8px"}
                     boxSize="50px"
                     objectFit="cover"
                     src={utilService.getURLImageUploadPresent()}
@@ -61,31 +62,35 @@ const StatisticCustomer = () => {
                     color="dark.200"
                     letterSpacing="2px"
                   >
-                    <Badge fontSize="xs" colorScheme="teal">
-                      {"ID >> 1"}
+                    <Badge
+                      fontSize="xs"
+                      colorScheme="teal"
+                      className="none-text-transform"
+                    >
+                      {"@username"}
                     </Badge>
                     <br />
-                    {"Tên sản phẩm"}
+                    {"Trần Cương Quyết"}
                   </Text>
                 </HStack>
-                <Text color="teal" fontWeight={"bold"}>
-                  10 đã bán
-                </Text>
+                {/* <Text color="teal" fontSize="sm" fontWeight={"bold"}>
+              10 đơn hàng thành công
+            </Text> */}
               </HStack>
             </Card>
           </HStack>
         </Card>
         <Card flex="1" p="4">
-          <Text fontSize="xl">Top sản phẩm yêu thích</Text>
+          <Text fontSize="xl">Top khách hàng tiềm năng</Text>
           <Text fontSize="md" color="gray" fontStyle={"italic"}>
-            (trong tháng)
+            (theo tổng tiền)
           </Text>
           <HStack w="100%" mt="4">
             <Card w="100%" p="2">
               <HStack justifyContent={"space-between"}>
                 <HStack spacing="3" className="product-card">
-                  <Image
-                    borderRadius={"8px"}
+                  <Avatar
+                    // borderRadius={"8px"}
                     boxSize="50px"
                     objectFit="cover"
                     src={utilService.getURLImageUploadPresent()}
@@ -96,16 +101,20 @@ const StatisticCustomer = () => {
                     color="dark.200"
                     letterSpacing="2px"
                   >
-                    <Badge fontSize="xs" colorScheme="teal">
-                      {"ID >> 1"}
+                    <Badge
+                      fontSize="xs"
+                      colorScheme="teal"
+                      className="none-text-transform"
+                    >
+                      {"@username"}
                     </Badge>
                     <br />
-                    {"Tên sản phẩm"}
+                    {"Trần Cương Quyết"}
                   </Text>
                 </HStack>
-                <Text color="teal" fontWeight={"bold"}>
-                  10 đã bán
-                </Text>
+                {/* <Text color="teal" fontSize="sm" fontWeight={"bold"}>
+              10 đơn hàng thành công
+            </Text> */}
               </HStack>
             </Card>
           </HStack>
