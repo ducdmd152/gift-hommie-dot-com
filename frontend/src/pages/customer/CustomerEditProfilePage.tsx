c463f9e9a15f841b1f3ae44d79106ed4f0a43a17
DangThiemLoc <thiemloc02@gmail.com>
2023-06-26 16:40:14 +0700

    Bug

--- frontend/src/pages/customer/CustomerEditProfilePage.tsx
+++ frontend/src/pages/customer/CustomerEditProfilePage.tsx
@@ -20,11 +20,9 @@ import { useState, useEffect } from "react";
 import UserDTO from "../../type/UserDTO";
 import { FieldValues, useForm } from "react-hook-form";
 
-interface FormData extends UserDTO {}
 
 const CustomerEditProfilePage = () => {
   const [customer, setCustomer] = useState<UserDTO>({} as UserDTO);
-  const navigate = useNavigate();
 
   useEffect(() => {
     customerService
@@ -43,34 +41,14 @@ const CustomerEditProfilePage = () => {
       });
   }, []);
 
-  // const {
-  //     register,
-  //     handleSubmit,
-  //     formState: { errors, isValid },
-  // } = useForm<FormData>();
-
-  // const onSubmit = (data: FieldValues) => {
-  //     const updateCustomer = data as UserDTO;
-  //     updateCustomer.id = customer.id;
-  //     console.log(updateCustomer);
-
-  //     customerService
-  //         .update(updateCustomer)
-  //         .then(() => {
-  //             navigate("/account");
-  //         })
-  //         .catch(() => {
-  //             alert(`Không thể sửa thông tin của "${customer.username}".\n Vui lòng thử lại.`);
-  //         });
-  // };
 
   return (
     <>
-      {/* <form onSubmit={handleSubmit(onSubmit)}> */}
+
       <Card marginX="200" marginY="6" p="8" border="1px lightgray solid">
         <UserProfileEdit userDTO={customer} />
       </Card>
-      {/* </form> */}
+
     </>
   );
 };
