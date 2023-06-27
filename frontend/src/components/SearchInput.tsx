f6f6ed266e3765d63aee4e9862d243d64809ff30
Duc Dao <90080923+ducdmd152@users.noreply.github.com>
2023-06-27 08:54:30 +0700

    Update SearchInput.tsx

--- frontend/src/components/SearchInput.tsx
+++ frontend/src/components/SearchInput.tsx
@@ -11,6 +11,7 @@ const SearchInput = ({ onSearch, defaultText }: Props) => {
   const ref = useRef<HTMLInputElement>(null);
   return (
     <form
+      onSubmit={(e) => e.preventDefault()}
       onChange={(event) => {
         event.preventDefault();
         if (ref.current) onSearch(ref.current.value);
