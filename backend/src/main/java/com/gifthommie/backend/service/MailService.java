ef2944da25a9a69c5fa04bbc56e1bf7e46910e34
QuyetTran1112 <133133145+QuyetTran1112@users.noreply.github.com>
2023-07-05 03:50:48 +0700

    Update Template Confomr ortder

--- backend/src/main/java/com/gifthommie/backend/service/MailService.java
+++ backend/src/main/java/com/gifthommie/backend/service/MailService.java
@@ -2,7 +2,7 @@ package com.gifthommie.backend.service;
 
 import java.util.Map;
 
-public interface ThymeleafService {
+public interface MailService {
 
 	 String createContent(String template, Map<String, Object> variables);
 	
