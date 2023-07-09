5698b0c6d499407d91c79694d523590e769db3e2
Duc Dao <90080923+ducdmd152@users.noreply.github.com>
2023-07-09 13:16:38 +0700

    Cate Mana | UI => add content

--- frontend/src/components/staff/staff-product-list-page/CategoryManagement.tsx
+++ frontend/src/components/staff/staff-product-list-page/CategoryManagement.tsx
@@ -1,12 +1,19 @@
 import {
+  Badge,
   Box,
   Button,
+  Card,
+  HStack,
+  Heading,
   Modal,
   ModalBody,
   ModalCloseButton,
   ModalContent,
   ModalFooter,
   ModalOverlay,
+  SimpleGrid,
+  Switch,
+  Text,
   useDisclosure,
 } from "@chakra-ui/react";
 import React from "react";
@@ -28,11 +35,49 @@ const CategoryManagement = () => {
         <ModalContent
           maxH="80vh"
           maxW="80vw"
+          padding="8"
           style={{ overflowY: "scroll", height: "90vh" }}
         >
           {/* <ModalHeader>Chi tiết đơn hàng</ModalHeader> */}
           <ModalCloseButton zIndex={4} />
-          <ModalBody>dfsafsaf</ModalBody>
+          <ModalBody>
+            <Box>
+              <Heading size="xl">Quản lý danh mục</Heading>
+              <SimpleGrid columns={2} spacing={10}>
+                <Card p="4" bg="teal.100">
+                  <HStack
+                    justifyContent={"space-between"}
+                    alignItems={"center"}
+                  >
+                    <Text size="xl">Danh mục số 1</Text>
+                    <HStack justifyContent={"space-between"} spacing={4}>
+                      <Switch size="lg" />
+                      <Badge
+                        fontSize={"sm"}
+                        variant="outline"
+                        className="badge-button none-text-transform"
+                        colorScheme="red"
+                        borderRadius={"4px"}
+                        paddingX="4"
+                        paddingY="4px"
+                        fontWeight={"500"}
+                        _hover={{
+                          background: "red.200",
+                        }}
+                      >
+                        Xóa
+                      </Badge>
+                    </HStack>
+                  </HStack>
+                </Card>
+                <Card p="4" bg="teal.100">
+                  <HStack justifyContent={"space-between"}>
+                    <Text size="xl">Danh mục số 1</Text>
+                  </HStack>
+                </Card>
+              </SimpleGrid>
+            </Box>
+          </ModalBody>
 
           <ModalFooter>
             <Button colorScheme="blue" mr={3} onClick={onClose}>
