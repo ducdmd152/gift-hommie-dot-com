98d5c20a8cfbb0390be4b5d8ef62cd6acbe83fd8
Duc Dao <90080923+ducdmd152@users.noreply.github.com>
2023-07-14 15:53:57 +0700

    Fixed search products hide/show pagination

--- frontend/src/components/staff/staff-product-list-page/StaffProductListMain.tsx
+++ frontend/src/components/staff/staff-product-list-page/StaffProductListMain.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import SearchInput from "../../SearchInput";
-import { Box, HStack } from "@chakra-ui/react";
+import { Box, Card, HStack, Text } from "@chakra-ui/react";
 import StaffProductListItems from "./StaffProductListItems";
 import Pagination from "../../Pagination";
 import useFetchStaffProduct, {
@@ -31,19 +31,29 @@ const StaffProductMain = ({
         }}
       />
 
-      <StaffProductListItems
-        products={products}
-        setCurrentProductId={setCurrentProductId}
-      />
-
-      <HStack justifyContent={"center"} mt={4} mb={8}>
-        <Pagination
-          pageable={pageable}
-          onSelectPageIndex={(index: number) =>
-            setStaffProductQuery({ ...staffProductQuery, page: index })
-          }
-        />
-      </HStack>
+      {!isLoading && products?.length > 0 && (
+        <>
+          <StaffProductListItems
+            products={products}
+            setCurrentProductId={setCurrentProductId}
+          />
+          <HStack justifyContent={"center"} mt={4} mb={8}>
+            <Pagination
+              pageable={pageable}
+              onSelectPageIndex={(index: number) =>
+                setStaffProductQuery({ ...staffProductQuery, page: index })
+              }
+            />
+          </HStack>
+        </>
+      )}
+      {!isLoading && products?.length == 0 && (
+        <Card width={"100%"} p="4" mt="2">
+          <Text size="lg" textAlign="center">
+            Không có sản phẩm nào trùng khớp.
+          </Text>
+        </Card>
+      )}
     </Box>
   );
 };
