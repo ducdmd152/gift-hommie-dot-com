c80cb02e5fe46d238bb90c9faf8f2f4f0f375f2f
QuyetTran1112 <133133145+QuyetTran1112@users.noreply.github.com>
2023-07-16 00:12:38 +0700

    Update StaffOrderController.java

--- backend/src/main/java/com/gifthommie/backend/controller/StaffOrderController.java
+++ backend/src/main/java/com/gifthommie/backend/controller/StaffOrderController.java
@@ -1,8 +1,18 @@
 package com.gifthommie.backend.controller;
 
+import java.nio.charset.StandardCharsets;
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.mail.internet.MimeMessage;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -13,9 +23,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.gifthommie.backend.dto.APIPageableResponseDTO;
 import com.gifthommie.backend.dto.OrderDTO;
+import com.gifthommie.backend.entity.OrderDetail;
 import com.gifthommie.backend.entity.Orders;
+import com.gifthommie.backend.entity.User;
 import com.gifthommie.backend.exception.NotFoundException;
+import com.gifthommie.backend.repository.OrderRepository;
+import com.gifthommie.backend.service.MailService;
 import com.gifthommie.backend.service.OrderService;
+import com.gifthommie.backend.service.UserService;
 
 @RestController
 @RequestMapping("/staff/order")
@@ -26,6 +41,13 @@ public class StaffOrderController {
 	
 	@Autowired
 	JavaMailSender mailSender;
+	
+	@Autowired
+	UserService userService;
+	
+	
+	@Autowired
+    MailService mailService;
 	
 	/// ------- NOTICE ------------- ///
 		// Make sure name Orders Table in Database is Order
@@ -43,8 +65,7 @@ public class StaffOrderController {
     public APIPageableResponseDTO<OrderDTO> getOrderList(
     		@RequestParam(defaultValue = "0", name = "page") Integer pageNo,
             @RequestParam(defaultValue = "12", name = "size") Integer pageSize,
-            @RequestParam(name = "status", required = false) String status,
-            @RequestParam(name = "search", defaultValue = "") String search){
+            @RequestParam(name = "status", required = false) String status){
 //        User user = SecurityUtils.getPrincipal().getUser();
         //String email = user.getEmail();
 		if(status!=null && status.isEmpty())
@@ -52,11 +73,11 @@ public class StaffOrderController {
 		
 		if(status == null) {
 //			orderService.getOrderDTOList_noEmail(pageNo, pageSize);
-			return orderService.getOrderDTOList_noEmail_withSearch(pageNo, pageSize, search);
+			return orderService.getOrderDTOList_noEmail(pageNo, pageSize);
 		}
 		
-		orderService.getOrderDTOList_noEmail_withSearch(0, 100, status, search); // FIRST-SHOT FOR AUTO-UPDATE
-		return orderService.getOrderDTOList_noEmail_withSearch(pageNo, pageSize, status, search); // SECOND-SHOT FOR RETURN
+		orderService.getOrderDTOList_noEmail(0, 100, status); // FIRST-SHOT FOR AUTO-UPDATE
+		return orderService.getOrderDTOList_noEmail(pageNo, pageSize, status); // SECOND-SHOT FOR RETURN
     }
 		
 		@GetMapping("/{orderId}")
@@ -69,6 +90,19 @@ public class StaffOrderController {
 			return orderDTO;
 		}
 		
+//	@PutMapping("/{orderID}")
+//	public void updateOrderState(@PathVariable int orderID,@RequestParam int status,@RequestParam String comment) {
+//		
+//		String STATUS[] = {"PENDING","CONFIRMED","DELIVERYING","SUCCESSFUL","CANCELED"};
+//		Orders order = orderService.getOrderByOrderId(orderID);
+//		if (order == null)
+//			throw new NotFoundException("ORDER CANNOT BE FOUND");
+//		if (order.getStatus().equals(STATUS[0]) || order.getStatus().equals(STATUS[1]) || order.getStatus().equals(STATUS[2])) {
+//			order.setStatus(STATUS[status]);
+//			order.setComment(comment);
+//			orderService.save(order);
+//		}
+//	}
 		//UPDATE ORDER
 		@PutMapping("/{orderId}")
 		public OrderDTO updateOrder(@PathVariable Integer orderId, 
@@ -97,22 +131,38 @@ public class StaffOrderController {
 		
 //		@Async
 		public void sendEmailConfirmOrder (Orders order) {	
+			User u = new User();
 			try {
-			
-				SimpleMailMessage message = new SimpleMailMessage();
-				    message.setTo(order.getEmail());
-				    message.setSubject("Đơn Hàng Mã Số #" + order.getId() + (order.getStatus().equals("CONFIRMED") ? " Đã Được Xác Nhận" : " Đã Bị Từ Chối"));
-
-				    message.setText("Chào bạn,\n\nĐơn Hàng Mã Số #" + order.getId() + (order.getStatus().equals("CONFIRMED") ? " Đã Được Xác Nhận" : " Đã Bị Từ Chối")
-				    		+ "\n\nĐơn hàng của bạn được cập nhật vào lúc: " +    order.getLastUpdatedTime()  + " ."
-				    		+ (order.getStatus().equals("CONFIRMED") ? "\n\nNhân viên của shop đang chuẩn bị hàng và nhanh chóng gửi đi\n\n" + "Thời gian giao hàng dự kiến cho đơn hàng của bạn là : "+ order.getExpectedDeliveryTime()  +"."
-				    					: "Lí do từ chối: Sản phẩm đang tạm hết hàng, chúng tôi sẽ liên hệ trong thời gian sớm nhất, xin lỗi vì sự bất tiện này!") 
-				    		
-				    		+ "\n\nCảm ơn bạn đã mua hàng tại cửa hàng của chúng tôi"
-				    		+ "\n\nTrân trọng,\nHommieStore");
-				    mailSender.send(message);			
-			} catch (Exception e) {
+				
+					MimeMessage message = mailSender.createMimeMessage();
+					MimeMessageHelper helper = new MimeMessageHelper(message,
+												MimeMessageHelper.MULTIPART_MODE_MIXED_RELATED,
+												StandardCharsets.UTF_8.name());
+					u = userService.getUserByEmail(order.getEmail());
+					// Set người gửi, người nhận
+					helper.setFrom("quyettcse160862@fpt.edu.vn");
+					helper.setTo(order.getEmail());
+					helper.setSubject("Đơn Hàng Mã Số #" + order.getId() + (order.getStatus().equals("CONFIRMED") ? " Đã Được Xác Nhận" : " Đã Bị Từ Chối"));
+				    
+					String confirmed_refuse = (order.getStatus().equals("CONFIRMED") 
+							? "Đã Được Xác Nhận \nĐơn hàng của bạn được cập nhật vào lúc: "+ order.getLastUpdatedTime()  + "." 
+							: "Đã Bị Từ Chối");
+					String order_status_info = (order.getStatus().equals("CONFIRMED") 
+							? "Nhân viên của shop đang chuẩn bị hàng và nhanh chóng gửi đi" + "\nThời gian giao hàng dự kiến cho đơn hàng của bạn là : "
+								+ ""+ order.getExpectedDeliveryTime()  +"."
+							: "Sản phẩm đang tạm hết hàng, chúng tôi sẽ liên hệ trong thời gian sớm nhất, xin lỗi vì sự bất tiện này!");
 					
+					// Truyền dữ liệu vào email
+					Map<String, Object> variables = new HashMap<>();
+					 	variables.put("user_name", u.getLastName());
+					 	variables.put("order_id", order.getId());
+			            variables.put("confirmed_refuse", confirmed_refuse);
+			            variables.put("order_status_info", order_status_info);
+			           
+			        helper.setText(mailService.createContent("conform-order.html", variables), true);
+					mailSender.send(message);    
+			} catch (Exception e) {
+				
 			}
 			
 		
